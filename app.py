e71caff4b84184094e45cd8a2881a7eebafc139a
Gaëtan <59415807+gaetanbrison@users.noreply.github.com>
2021-10-31 20:49:28 +0100

    import streamlit_pandas_profiling

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-from PIL import Image 
+from PIL import Image
+from streamlit_pandas_profiling import st_profile_report
 
 
     
